ded48c13c67b48613a26516d6d65677d77e4efb6
a.balashov <write.anton.balashov@gmail.com>
2023-02-19 17:33:48 +0300

    e2k

--- platform.go
+++ platform.go
@@ -57,128 +57,12 @@ func addDrop(base []Platform, add []Platform, drop []Platform) []Platform {
 
 var (
 	Platforms_1_0 = []Platform{
-		{"darwin", "386", true},
-		{"darwin", "amd64", true},
-		{"linux", "386", true},
-		{"linux", "amd64", true},
-		{"linux", "arm", true},
-		{"freebsd", "386", true},
-		{"freebsd", "amd64", true},
-		{"openbsd", "386", true},
-		{"openbsd", "amd64", true},
-		{"windows", "386", true},
-		{"windows", "amd64", true},
+
 		{"linux", "e2k", true},
 	}
 
-	Platforms_1_1 = addDrop(Platforms_1_0, []Platform{
-		{"freebsd", "arm", true},
-		{"netbsd", "386", true},
-		{"netbsd", "amd64", true},
-		{"netbsd", "arm", true},
-		{"plan9", "386", false},
-	}, nil)
-
-	Platforms_1_3 = addDrop(Platforms_1_1, []Platform{
-		{"dragonfly", "386", false},
-		{"dragonfly", "amd64", false},
-		{"nacl", "amd64", false},
-		{"nacl", "amd64p32", false},
-		{"nacl", "arm", false},
-		{"solaris", "amd64", false},
-	}, nil)
-
-	Platforms_1_4 = addDrop(Platforms_1_3, []Platform{
-		{"android", "arm", false},
-		{"plan9", "amd64", false},
-	}, nil)
-
-	Platforms_1_5 = addDrop(Platforms_1_4, []Platform{
-		{"darwin", "arm", false},
-		{"darwin", "arm64", false},
-		{"linux", "arm64", false},
-		{"linux", "ppc64", false},
-		{"linux", "ppc64le", false},
-	}, nil)
-
-	Platforms_1_6 = addDrop(Platforms_1_5, []Platform{
-		{"android", "386", false},
-		{"android", "amd64", false},
-		{"linux", "mips64", false},
-		{"linux", "mips64le", false},
-		{"nacl", "386", false},
-		{"openbsd", "arm", true},
-	}, nil)
-
-	Platforms_1_7 = addDrop(Platforms_1_5, []Platform{
-		// While not fully supported s390x is generally useful
-		{"linux", "s390x", true},
-		{"plan9", "arm", false},
-		// Add the 1.6 Platforms, but reflect full support for mips64 and mips64le
-		{"android", "386", false},
-		{"android", "amd64", false},
-		{"linux", "mips64", true},
-		{"linux", "mips64le", true},
-		{"nacl", "386", false},
-		{"openbsd", "arm", true},
-	}, nil)
-
-	Platforms_1_8 = addDrop(Platforms_1_7, []Platform{
-		{"linux", "mips", true},
-		{"linux", "mipsle", true},
-	}, nil)
-
-	// no new platforms in 1.9
-	Platforms_1_9 = Platforms_1_8
-
-	// unannounced, but dropped support for android/amd64
-	Platforms_1_10 = addDrop(Platforms_1_9, nil, []Platform{{"android", "amd64", false}})
-
-	Platforms_1_11 = addDrop(Platforms_1_10, []Platform{
-		{"js", "wasm", true},
-	}, nil)
-
-	Platforms_1_12 = addDrop(Platforms_1_11, []Platform{
-		{"aix", "ppc64", false},
-		{"windows", "arm", true},
-	}, nil)
-
-	Platforms_1_13 = addDrop(Platforms_1_12, []Platform{
-		{"illumos", "amd64", false},
-		{"netbsd", "arm64", true},
-		{"openbsd", "arm64", true},
-	}, nil)
-
-	Platforms_1_14 = addDrop(Platforms_1_13, []Platform{
-		{"freebsd", "arm64", true},
-		{"linux", "riscv64", true},
-	}, []Platform{
-		// drop nacl
-		{"nacl", "386", false},
-		{"nacl", "amd64", false},
-		{"nacl", "arm", false},
-	})
-
-	Platforms_1_15 = addDrop(Platforms_1_14, []Platform{
-		{"android", "arm64", false},
-	}, []Platform{
-		// drop i386 macos
-		{"darwin", "386", false},
-	})
-
-	Platforms_1_16 = addDrop(Platforms_1_15, []Platform{
-		{"android", "amd64", false},
-		{"darwin", "arm64", true},
-		{"openbsd", "mips64", false},
-	}, nil)
-
-	Platforms_1_17 = addDrop(Platforms_1_16, []Platform{
-		{"windows", "arm64", true},
-		{"linux", "e2k", true},
-	}, nil)
-
 	// no new platforms in 1.18
-	Platforms_1_18 = Platforms_1_17
+	Platforms_1_18 = Platforms_1_0
 
 	PlatformsLatest = Platforms_1_18
 )
@@ -205,23 +89,8 @@ func SupportedPlatforms(v string) []Platform {
 		constraint string
 		plat       []Platform
 	}{
-		{"<= 1.0", Platforms_1_0},
-		{">= 1.1, < 1.3", Platforms_1_1},
-		{">= 1.3, < 1.4", Platforms_1_3},
-		{">= 1.4, < 1.5", Platforms_1_4},
-		{">= 1.5, < 1.6", Platforms_1_5},
-		{">= 1.6, < 1.7", Platforms_1_6},
-		{">= 1.7, < 1.8", Platforms_1_7},
-		{">= 1.8, < 1.9", Platforms_1_8},
-		{">= 1.9, < 1.10", Platforms_1_9},
-		{">= 1.10, < 1.11", Platforms_1_10},
-		{">= 1.11, < 1.12", Platforms_1_11},
-		{">= 1.12, < 1.13", Platforms_1_12},
-		{">= 1.13, < 1.14", Platforms_1_13},
-		{">= 1.14, < 1.15", Platforms_1_14},
-		{">= 1.15, < 1.16", Platforms_1_15},
-		{">= 1.16, < 1.17", Platforms_1_16},
-		{">= 1.17, < 1.18", Platforms_1_17},
+
+		{">= 1.17, < 1.18", Platforms_1_18},
 		{">= 1.18, < 1.19", Platforms_1_18},
 	}
 
